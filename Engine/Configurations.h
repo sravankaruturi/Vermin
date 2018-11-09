c33fa19669bfc766f5acc57191dd20346fb96154
sravankaruturi <knight2356@gmail.com>
2018-11-08 22:03:10 -0500

    Added Configurations to the ignore file

--- Engine/Configurations.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define            TESTING_ONLY            0
-#define            DISABLE_UNIT_TESTS        1
-#define            ENABLE_GUI                0
-#define            IS_HOME_PC                1
-#define            MACBOOK                   0
-#define               LAB_MACHINE                    0
-#define            RELATIVE_PATHS            FALSE
