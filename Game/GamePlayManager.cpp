126c066a679155938cdd2f24f4ff52a75e0661b9
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2018-11-11 23:53:03 -0500

    Added the Project Design Document
    It is just a vim txt document

---
+++ Game/GamePlayManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sravan Karuturi on 11/11/2018.
+//
+
+#include "GamePlayManager.h"
---
+++ Game/GamePlayManager.h
@@ -0,0 +1,21 @@
+//
+// Created by Sravan Karuturi on 11/11/2018.
+//
+
+#ifndef GAME_GAMEPLAYMANAGER_H
+#define GAME_GAMEPLAYMANAGER_H
+
+
+class GamePlayManager {
+
+private: 
+
+
+public:
+
+	static const int max_population = 20;
+
+};
+
+
+#endif //GAME_GAMEPLAYMANAGER_H
