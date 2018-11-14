ed4d0b9f7f1d1455ba2ec5d926f734bf045ea449
Sravan Kumar Karuturi (RIT Student) <skk7174@ad.rit.edu>
2018-11-14 17:20:50 -0500

    Update Game/Unit.cpp
    Updated the Bounding box to fit the Character.

--- Game/Unit.cpp
+++ Game/Unit.cpp
@@ -7,10 +7,10 @@
 namespace v_game {
 
 	Unit::Unit()
-		: AnimatedEntity("Unit", "RTSDemo/HappyIdle.fbx", "unit")
+		: AnimatedEntity("Unit", "RTSDemo/HappyIdle.fbx", "unit", glm::vec3(-10, 0, -10), glm::vec3(10, 50, 10))
 	{
 
-		this->SetScale(glm::vec3(1 / 32.0f));
+		this->SetScale(glm::vec3(1 / 64.0f));
 		this->SetRotation(glm::vec3(0, 0, 0));
 
 	}
