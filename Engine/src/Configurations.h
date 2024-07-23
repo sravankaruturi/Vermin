1ed9b9df90c7c720385bbd1c34faa98af3414cbb
Sravan <sravan.karuturi@strivr.com>
2024-07-22 22:47:21 -0400

    Adding the configurations file to prevent compilation issues

---
+++ Engine/src/Configurations.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define			TESTING_ONLY			0
+#define			DISABLE_UNIT_TESTS		1
+#define			ENABLE_GUI				1
+#define			IS_HOME_PC				0
+#define			MACBOOK					1
+#define			LAB_MACHINE				0
+#define			RELATIVE_PATHS			FALSE
